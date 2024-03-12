61277ec582dfa2b8a0ff7a1add38f426199a75d7
mndgs1 <mindaugasbankauskas@gmail.com>
2024-03-12 17:39:38 +0100

    changed in api

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,18 +5,16 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if (cached.conn) {
-        return cached.conn;
-    }
+    if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) {
-        throw new Error("MONGODB_URI is not defined");
-    }
+    if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-    cached.promise = mongoose.connect(MONGODB_URI, {
-        dbName: "Evently",
-        bufferCommands: false,
-    });
+    cached.promise =
+        cached.promise ||
+        mongoose.connect(MONGODB_URI, {
+            dbName: "Evently",
+            bufferCommands: false,
+        });
 
     cached.conn = await cached.promise;
 
